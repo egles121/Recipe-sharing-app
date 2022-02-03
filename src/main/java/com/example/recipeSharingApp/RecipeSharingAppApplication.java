111881fb999a774730e9a6e5cef763b812dd7c11
egle.simniskyte <egle.simniskyte@accenture.com>
2022-02-03 11:23:46 +0200

    added Spring project

---
+++ src/main/java/com/example/recipeSharingApp/RecipeSharingAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.recipeSharingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeSharingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeSharingAppApplication.class, args);
+	}
+
+}
